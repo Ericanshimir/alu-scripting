1e91c34328acb49964c0e5411170828a7aeae2b4
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2023-03-06 20:27:59 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,12 +3,14 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        posts = data["data"]["children"]
+        for i in range(min(len(posts), 10)):
+            print(posts[i]["data"]["title"])
     else:
-        return 0
+        print(None)
