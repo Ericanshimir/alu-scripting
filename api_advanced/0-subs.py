cff179bd66b3303e5d768d666d19417df2495195
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2023-03-06 20:20:33 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-'''how many subs'''
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """ number_of_subscribers """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
+        return response.json()["data"]["subscribers"]
     else:
         return 0
